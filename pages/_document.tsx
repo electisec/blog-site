ce0379e8fe7d6852efcc026b466bb7e31dbd8046
nullity00 <reshmashaik3011@gmail.com>
2025-01-05 20:04:03 +0400

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -47,7 +48,21 @@ export default function Document() {
       </Head>
       <body>
         <Main />
+
         <NextScript />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q647YTPGSE"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Q647YTPGSE');
+          `}
+        </Script>
       </body>
     </Html>
   );
