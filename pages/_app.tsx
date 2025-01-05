dc383f21a5dd5c490b99fbea410b5d6e9a27c897
nullity00 <reshmashaik3011@gmail.com>
2025-01-05 19:19:23 +0400

    metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Navbar from '../components/Navbar';
-import { Space_Grotesk } from 'next/font/google'
+import Navbar from "../components/Navbar";
+import { Space_Grotesk } from "next/font/google";
 import { Metadata } from "next";
 import Footer from "@/components/Footer";
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-})
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Electisec Blogs (previously yAcademy & yAudit)",
@@ -20,13 +20,13 @@ export const metadata: Metadata = {
   robots: "follow, index",
   openGraph: {
     type: "website",
-    url: "https://blogs.electisec.dev",
+    url: "https://blogs.electisec.tech",
     title: "Electisec (previously yAcademy & yAudit)",
     description: "ZK & Smart Contract Security",
     siteName: "Electisec",
     images: [
       {
-        url: "https://blogs.electisec.dev/logo.svg",
+        url: "https://blogs.electisec.tech/logo.svg",
       },
     ],
   },
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     site: "@electisec",
     creator: "@electisec",
-    images: "https://electisec.dev/logo.svg",
+    images: "https://electisec.tech/logo.svg",
   },
 };
 
