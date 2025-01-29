8d23c05a1779c9a52a6d51d424f597046594c631
nullity00 <reshmashaik3011@gmail.com>
2025-01-29 17:55:15 +0400

    syntax highlightening except solidity

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,7 @@ import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkRehype from 'remark-rehype';
+import rehypeHighlight from 'rehype-highlight';
 import rehypeStringify from 'rehype-stringify';
 import { visit } from 'unist-util-visit';
 import { clsx, type ClassValue } from "clsx"
@@ -28,26 +29,14 @@ export function extractDate(filename: string): string | null {
   return new Date().toISOString();
 }
 
-// Custom plugin to wrap math blocks in centered divs
-function remarkWrapMath() {
+function remarkCodeBlocks() {
   return (tree: any) => {
-    visit(tree, ['math', 'inlineMath'], (node: any) => {
-      if (node.type === 'math') {
-        // Split equations by alignment markers
-        const equations = node.value.split(/\\[^\\]/).filter(Boolean);
-        
-        if (equations.length > 1) {
-          // Multiple equations - create an align environment
-          node.value = `\\begin{align*}\n${equations.join('\\\\\n')}\n\\end{align*}`;
-        }
-        
-        // Wrap display math in a centered div with proper spacing
-        node.data = node.data || {};
-        node.data.hProperties = {
-          className: 'math-display-wrapper',
-          style: 'display: block; text-align: center; margin: 2rem auto; width: 100%;'
-        };
-      }
+    visit(tree, 'code', (node: any) => {
+      // Add a pre class that highlight.js can target
+      node.data = node.data || {};
+      node.data.hProperties = {
+        className: `language-${node.lang || 'text'}`,
+      };
     });
   };
 }
@@ -63,7 +52,7 @@ export async function processMarkdown(content: string) {
     .use(remarkGfm)
     .use(remarkReplaceImageUrls)
     .use(remarkMath)
-    .use(remarkWrapMath)  // Add our custom plugin
+    .use(remarkCodeBlocks)
     .use(remarkRehype, {
       allowDangerousHtml: true,
     })
@@ -78,6 +67,7 @@ export async function processMarkdown(content: string) {
       throwOnError: false,
       displayMode: true,  // Force display mode for all equations
     })
+    .use(rehypeHighlight)
     .use(rehypeStringify, { allowDangerousHtml: true })
     .process(cleanedContent);
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Basic Metadata */}
         <meta charSet="utf-8" />
-
         <meta name="description" content="ZK & Smart Contract Security" />
         <meta
           name="keywords"
@@ -17,7 +15,6 @@ export default function Document() {
         <meta name="creator" content="Electisec Team" />
         <meta name="robots" content="follow, index" />
 
-        {/* Open Graph */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://blog.electisec.tech" />
         <meta
@@ -34,13 +31,11 @@ export default function Document() {
           content="https://blog.electisec.tech/logo.svg"
         />
 
-        {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@electisec" />
         <meta name="twitter:creator" content="@electisec" />
         <meta name="twitter:image" content="https://electisec.tech/logo.svg" />
 
-        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
@@ -48,11 +43,33 @@ export default function Document() {
           integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV"
           crossOrigin="anonymous"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css"
+        />
       </Head>
       <body>
         <Main />
-
         <NextScript />
+        
+        <Script
+          strategy="afterInteractive"
+          src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"
+        />
+        <Script
+          strategy="afterInteractive"
+          src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/solidity.min.js"
+        />
+        
+        {/* Initialize highlight.js after libraries are loaded */}
+        <Script id="highlight-init" strategy="afterInteractive">
+          {`
+            window.onload = function() {
+              hljs.highlightAll();
+            }
+          `}
+        </Script>
+
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-Q647YTPGSE"
